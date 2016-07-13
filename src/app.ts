526e0e00c61c0411101906019a8a3e5002d96f6a
dominikus1993 <dominikus1910@gmail.com>
2016-07-13 13:18:04 +0200

    added app file

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+/**
+ * Created by dominik.kotecki on 13-07-2016.
+ */
+"use strict";
+import {Request, Response} from "express";
+import * as express from "express"
+import * as path from "path"
+import * as logger from "morgan"
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+
+var app = express();
+
+app.use(logger(<any>"dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.use((req: Request, res: Response, next: Function) => {
+    var err: any = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+if (app.get('env') === 'development') {
+    app.use(function(err: any, req: Request, res: Response, next: Function) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err: any, req: Request, res: Response, next: Function) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
