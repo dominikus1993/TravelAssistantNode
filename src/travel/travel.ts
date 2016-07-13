d9d1c07f70541c73e8f594d7c0999d2e93c1979b
dominikus1993 <dominikus1910@gmail.com>
2016-07-13 22:28:07 +0200

    fix

--- src/travel/travel.ts
+++ src/travel/travel.ts
@@ -1,6 +1,6 @@
 ///<reference path="../../typings/index.d.ts"/>
 
-import {Document, Schema}  from "mongoose"
+import {Document, Schema, model}  from "mongoose"
 import {User} from "../users/user";
 
 
@@ -14,7 +14,6 @@ export interface Travel extends Document {
 }
 
 export interface Reservation extends Document {
-    id?:string;
     user:User;
     date:Date;
     travelType:ReservationType;
@@ -22,18 +21,27 @@ export interface Reservation extends Document {
 
 export type ReservationType = "IN" | "OUT"
 
-export const TravelSchema = new Schema({
+const TravelSchema = new Schema({
     from: String,
     to: String,
     date: Date,
-    owner: User,
+    owner: {type: Schema.Types.ObjectId, ref: "User"},
     passengers: [{type: Schema.Types.ObjectId, ref: "Reservation"}],
     placeLimit: Number,
 });
 
-export const ReservationSchema = new Schema({
-    id: String,
+const ReservationSchema = new Schema({
     user: [{type: Schema.Types.ObjectId, ref: "User"}],
     date: Date,
     travelType: {type: String, enum: ["IN", "OUT"]}
 });
+
+const UsersSchema = new Schema({
+    username: String,
+    email: String,
+    password: String
+});
+
+export class TravelRepository{
+
+}
--- src/users/user.ts
+++ src/users/user.ts
@@ -1,13 +1,7 @@
 ///<reference path="../../typings/index.d.ts"/>
-import {Document, Schema}  from "mongoose"
+import {Document, Schema, model}  from "mongoose"
 export interface User extends Document {
     username:string;
     email:string;
     password:string;
 }
-
-export const UserSchema = new Schema({
-    username: String,
-    email: String,
-    password: String
-});
