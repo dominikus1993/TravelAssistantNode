18f3861d4228e162eab304309f5f10b6754cffe8
dominikus1993 <dominikus1910@gmail.com>
2016-07-13 21:04:32 +0200

    added travel model

--- src/travel/travel.ts
+++ src/travel/travel.ts
@@ -1,4 +1,26 @@
+///<reference path="../../typings/index.d.ts"/>
+
 export module TravelAssistant.Travel.Models{
-    export interface Travel{
+    import * as mongoose from "mongoose"
+    import User = TravelAssistant.Users.Models.User;
+
+    export interface Travel extends mongoose.Document {
+        from : string;
+        to : string;
+        date : Date;
+        owner : User;
+        passengers : Reservation[];
+        placeLimit : number;
+    }
+
+    export interface Reservation extends mongoose.Document{
+        user : User;
+        date : Date;
+        type : ReservationType;
+    }
+
+    export enum ReservationType{
+        In,
+        Out
     }
 }
