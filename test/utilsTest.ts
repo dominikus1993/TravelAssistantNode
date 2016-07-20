dcd1bd0b55b25682bf2097bc2777d4e98a9ee09f
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-07-20 16:55:29 +0200

    added tests to Result functions

--- test/utilsTest.ts
+++ test/utilsTest.ts
@@ -1,5 +1,6 @@
 ///<reference path="../typings/index.d.ts"/>
 import {isNullOrUndefined} from "../src/global/utils";
+import {getResult, getError} from "../src/global/result";
 describe("test utils functions", () => {
     describe("test function that recognize if value is null or undefined", () => {
         it("value is null", () => {
@@ -14,4 +15,80 @@ describe("test utils functions", () => {
             expect(isNullOrUndefined(2)).toBeFalsy();
         });
     });
+
+    describe("test function that generate result", () => {
+        describe("put null to getResult function", () => {
+            const testResult = getResult(null);
+            it("isSuccess should be false", () => {
+                expect(testResult.isSuccess).toBeFalsy();
+            });
+            it("isError should be false", () => {
+                expect(testResult.isError).toBeFalsy();
+            });
+            it("value should be null", () => {
+                expect(testResult.value).toBeNull();
+            });
+            it("message should be null", () => {
+                expect(testResult.message).toBeNull();
+            });
+        });
+
+        describe("put undefined to getResult function", () => {
+            const testResult = getResult(undefined);
+            it("isSuccess should be false", () => {
+                expect(testResult.isSuccess).toBeFalsy();
+            });
+            it("isError should be false", () => {
+                expect(testResult.isError).toBeFalsy();
+            });
+            it("value should be undefined", () => {
+                expect(testResult.value).toBeUndefined();
+            });
+            it("message should be null", () => {
+                expect(testResult.message).toBeNull();
+            });
+        });
+
+        describe("put real value to getResult function", () => {
+            const testResult = getResult(2);
+            it("isSuccess should be true", () => {
+                expect(testResult.isSuccess).toBeTruthy();
+            });
+            it("isError should be false", () => {
+                expect(testResult.isError).toBeFalsy();
+            });
+            it("value should be false", () => {
+                expect(testResult.value).toEqual(2);
+            });
+            it("message should be null", () => {
+                expect(testResult.message).toBeNull();
+            });
+        });
+
+
+        describe("put error to getError message function", () => {
+            const testResult = getError({errmsg : "mess", code : 401});
+            it("isSuccess should be true", () => {
+                expect(testResult.isSuccess).toBeFalsy();
+            });
+            it("isError should be false", () => {
+                expect(testResult.isError).toBeTruthy();
+            });
+            it("value should be null", () => {
+                expect(testResult.value).toBeNull();
+            });
+            it("message should not be null", () => {
+                expect(testResult.message).not.toBeNull();
+
+                it("error message should not be null", () => {
+                    expect(testResult.message.errmsg).not.toBeNull()
+                });
+                it("code should equal 401", () => {
+                   expect(testResult.message.code).toEqual(401);
+                });
+            });
+        });
+    });
 });
+
+
