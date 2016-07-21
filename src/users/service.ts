fadd88df6912125df9497134a6e30b9163679825
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-07-21 14:22:49 +0200

    refactored checkAuth method

--- src/users/service.ts
+++ src/users/service.ts
@@ -44,35 +44,34 @@ export class UserService implements IUserService {
     }
 
     register(user: { username: string; email: string; password: string; passwordConfirm: string }): Promise<{}> {
-            if (user.password === user.passwordConfirm) {
-                return Promise.resolve(this.userRepository.register({
-                    login: user.username,
-                    email: user.email,
-                    password: user.password
-                })).then(fulfilled => {
-                    return getResult(fulfilled);
-                }, rejected => {
-                    return getError(rejected);
-                });
-            }
-            else {
-                return Promise.reject(getError({ code: 1, errmsg: "password confirm not equal to password" }));
-            }
+        if (user.password === user.passwordConfirm) {
+            return Promise.resolve(this.userRepository.register({
+                login: user.username,
+                email: user.email,
+                password: user.password
+            })).then(fulfilled => {
+                return getResult(fulfilled);
+            }, rejected => {
+                return getError(rejected);
+            });
+        }
+        else {
+            return Promise.reject(getError({ code: 1, errmsg: "password confirm not equal to password" }));
+        }
     }
 
     checkAuth(loginData: any) {
-        return new Promise((resolve: (val: Result<{}>) => void, reject: (error?: any) => void) => {
-            this.userRepository.getLoginData({ _id: loginData }).then((fulfilled: LoginData) => {
-                if (!isNullOrUndefined(fulfilled)) {
-                    resolve(getResult(fulfilled.expirationDate.getTime() > new Date().getTime()));
-                }
-                else {
-                    resolve(getResult(false));
-                }
-            }, rejected => {
-                resolve(getResult(false));
-            });
+        return Promise.resolve(this.userRepository.getLoginData({ _id: loginData })).then((fulfilled: LoginData) => {
+            if (!isNullOrUndefined(fulfilled)) {
+                return getResult(fulfilled.expirationDate.getTime() > new Date().getTime());
+            }
+            else {
+                return getResult(false);
+            }
+        }, rejected => {
+            return getResult(false);
         });
+
     }
 
 }
