fd0f40d33305c79845c179f37e06c937433d95d0
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-07-21 16:45:42 +0200

    refactoring by tslint again

--- src/app.ts
+++ src/app.ts
@@ -5,52 +5,47 @@ import * as path from "path";
 import * as logger from "morgan";
 import * as cookieParser from "cookie-parser";
 import * as bodyParser from "body-parser";
-import * as mongoose from "mongoose"
+import * as mongoose from "mongoose";
 import Promise = require("bluebird");
 import router from "./global/router";
 import {mongoDbUrl} from "./global/config";
+import {getError} from "./global/result";
 mongoose.Promise = Promise as any;
 mongoose.connect(mongoDbUrl);
 
 
-var app = express();
+const app = express();
 
-//noinspection TypeScriptValidateTypes
+// noinspection TypeScriptValidateTypes
 app.use(logger("dev" as any) as any);
-//noinspection TypeScriptValidateTypes
+// noinspection TypeScriptValidateTypes
 app.use(bodyParser.json());
-//noinspection TypeScriptValidateTypes
+// noinspection TypeScriptValidateTypes
 app.use(bodyParser.urlencoded({ extended: true }));
-//noinspection TypeScriptValidateTypes
+// noinspection TypeScriptValidateTypes
 app.use(cookieParser());
-//noinspection TypeScriptValidateTypes
+// noinspection TypeScriptValidateTypes
 app.use("/api", router);
-//noinspection TypeScriptValidateTypes
+// noinspection TypeScriptValidateTypes
 app.use((req: Request, res: Response, next: Function) => {
-    var err: any = new Error("Not Found");
+    let err: any = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
-//noinspection TypeScriptValidateTypes
+// noinspection TypeScriptValidateTypes
 if (app.get("env") === "development") {
-    //noinspection TypeScriptValidateTypes
+    // noinspection TypeScriptValidateTypes
     app.use(function(err: any, req: Request, res: Response, next: Function) {
         res.status(err.status || 500);
-        res.json({
-            message: err.message,
-            error: err
-        });
+        res.json(getError({code : 500, errmsg : err.message}));
     });
 }
 
-//noinspection TypeScriptValidateTypes
+// noinspection TypeScriptValidateTypes
 app.use(function(err: any, req: Request, res: Response, next: Function) {
     res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: {}
-    });
+    res.json(getError({code : 500, errmsg : err.message}));
 });
 
 
--- src/global/router.ts
+++ src/global/router.ts
@@ -1,7 +1,7 @@
 ///<reference path="../../typings/index.d.ts"/>
-import * as expressservestaticcore from "express-serve-static-core"
-import {all} from "../travel/controller"
-import {register, login, checkAuth} from  "../users/controller"
+import * as expressservestaticcore from "express-serve-static-core";
+import {all} from "../travel/controller";
+import {register, login, checkAuth} from  "../users/controller";
 import {Router} from "express";
 
 const router = Router();
