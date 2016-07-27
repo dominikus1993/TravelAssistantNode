1ea39c2f6667060698c4c500383c21b3a2b91d17
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-07-27 17:00:34 +0200

    added function that add header to all request

--- src/global/router.ts
+++ src/global/router.ts
@@ -4,7 +4,12 @@ import {Router} from "express";
 
 const router = Router();
 
-router.get("/travel/all", [checkAuth, all]);
+router.get("/*", (req,res,next) => {
+    res.header("Access-Control-Allow-Origin" , "*" );
+    next(); // http://expressjs.com/guide.html#passing-route control
+});
+
+router.get("/travel/all", all);
 router.post("/travel/create", [checkAuth, create]);
 
 // Auth
