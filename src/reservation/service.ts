d36350eb8dbdcdf092fc5d88005548abf561bfcd
dominikus1993 <dominikus1910@gmail.com>
2016-07-31 09:04:37 +0200

    added reservation service

---
+++ src/reservation/service.ts
@@ -0,0 +1,13 @@
+import Promise = require("bluebird");
+import {getError, getResult, Result} from "../global/result";
+import {encrypt, isNullOrUndefined} from "../global/utils";
+import {IReservationRepository} from "./repository";
+
+
+export interface IReservationService {
+
+}
+
+export class ReservationService implements IReservationRepository {
+    
+}
