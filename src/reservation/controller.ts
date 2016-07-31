1d3f5e5559dc2dd74bb5c8f338eea694599edc3e
dominikus1993 <dominikus1910@gmail.com>
2016-07-31 09:05:27 +0200

    added controller

---
+++ src/reservation/controller.ts
@@ -0,0 +1,4 @@
+import * as status from "../global/constants";
+import {Result, getError} from "../global/result";
+import {ReservationRepository} from "./repository";
+import {ReservationService} from "./service";
