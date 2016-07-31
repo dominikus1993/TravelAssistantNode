320b634bb882ed227cfa5bc1e4789fd3125819b0
dominikus1993 <dominikus1910@gmail.com>
2016-07-31 09:04:01 +0200

    added reservation repository

---
+++ src/reservation/repository.ts
@@ -0,0 +1,11 @@
+import {encrypt} from "../global/utils";
+import * as Promise from "bluebird";
+import {Model}  from "mongoose";
+
+export interface IReservationRepository {
+
+}
+
+export class ReservationRepository implements IReservationRepository {
+
+};
