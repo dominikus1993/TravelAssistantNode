fe4c06d2d369db48636da4a72c0c26489cfdfc33
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-08-01 16:40:54 +0200

    added save method to reservation service

--- src/reservation/service.ts
+++ src/reservation/service.ts
@@ -1,13 +1,25 @@
 import Promise = require("bluebird");
 import {getError, getResult, Result} from "../global/result";
-import {encrypt, isNullOrUndefined} from "../global/utils";
 import {IReservationRepository} from "./repository";
+import {Reservation} from "./model";
 
 
 export interface IReservationService {
-
+    save(reservation: Reservation): Promise<any>;
 }
 
-export class ReservationService implements IReservationRepository {
-    
-}
+export class ReservationService implements IReservationService {
+
+    constructor(private respository: IReservationRepository) {
+
+    }
+
+    public save(reservation: Reservation): Promise<Result<any, Error>> {
+        return this.respository.save(reservation).then(fulfilled => {
+            return getResult(fulfilled);
+        }, rejected => {
+            return getError(new Error(rejected));
+        });
+    }
+
+};
