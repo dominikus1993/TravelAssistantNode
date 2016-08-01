36690a6572b1beb312bec7d5f00a97da22810e93
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-08-01 16:36:58 +0200

    added save method to reservation repository

--- src/reservation/repository.ts
+++ src/reservation/repository.ts
@@ -1,11 +1,19 @@
 import {encrypt} from "../global/utils";
+import {Reservation} from "./model";
 import * as Promise from "bluebird";
 import {Model}  from "mongoose";
 
 export interface IReservationRepository {
-
+    save(data: Reservation): Promise<Reservation>;
 }
 
 export class ReservationRepository implements IReservationRepository {
 
+    constructor(private model: Model<Reservation>){
+
+    }
+
+    save(data: Reservation): Promise<Reservation> {
+        return new this.model(data).save() as any as Promise<Reservation>;
+    }
 };
