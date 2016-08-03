7374cc39c5902511d6d158bdbb6821a94f0c15d3
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-08-03 16:48:12 +0200

    added findby method to reservation repository

--- src/reservation/repository.ts
+++ src/reservation/repository.ts
@@ -4,6 +4,7 @@ import {Model}  from "mongoose";
 
 export interface IReservationRepository {
     save(data: Reservation): Promise<Reservation>;
+    findBy(by: Object): Promise<Reservation[]>;
 }
 
 export class ReservationRepository implements IReservationRepository {
@@ -15,5 +16,8 @@ export class ReservationRepository implements IReservationRepository {
     public save(data: Reservation): Promise<Reservation> {
         return new this.model(data).save() as any as Promise<Reservation>;
     }
-};
 
+    public findBy(by: Object): Promise<Reservation[]> {
+        return this.model.find(by).exec() as any as Promise<Reservation[]>;
+    }
+}
