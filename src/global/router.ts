dc6495dac7b7b6c51954e4d089cc30f900a29098
dominikus1993 <dominikus1910@gmail.com>
2016-08-04 20:24:01 +0200

    added headers in routing

--- src/global/router.ts
+++ src/global/router.ts
@@ -5,7 +5,10 @@ import {Router} from "express";
 const router = Router();
 
 router.get("/*", (req, res, next) => {
-    res.header("Access-Control-Allow-Origin" , "*" );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+    res.header("Access-Control-Allow-Credentials", "true")
     next(); // http://expressjs.com/guide.html#passing-route control
 });
 
